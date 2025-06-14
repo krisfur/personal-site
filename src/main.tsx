4dea6d82cd3c707e538a5941458e5ba2cf601db8
Krzysztof Furman <k_furman@outlook.com>
2025-06-14 17:00:49 +0100

    Removed unused debug message.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import './App.css';
 import App from './App.tsx'
 
-console.log('Tailwind loaded? test', window.getComputedStyle(document.body).backgroundColor);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
